91b54400cfd665e5b548c330fa700ee0efb25c30
Alexis Yushin <alexis@apexive.com>
2023-09-11 12:03:14 +0200

    moved SOCIAL_AUTH_PIPELINE to defaults

--- django/simple_auth/settings.py
+++ django/simple_auth/settings.py
@@ -6,7 +6,8 @@ from df_api_drf.defaults import (
     SPECTACULAR_SETTINGS,
 )
 
-from df_auth.defaults import DF_AUTH_INSTALLED_APPS
+from df_auth.defaults import DF_AUTH_INSTALLED_APPS, SOCIAL_AUTH_PIPELINE
+
 
 
 
@@ -125,36 +126,3 @@ SPECTACULAR_SETTINGS = {**SPECTACULAR_SETTINGS}
 SOCIAL_AUTH_FACEBOOK_KEY = "xxxxxxx"
 SOCIAL_AUTH_FACEBOOK_SECRET = "xxxxxxx"
 
-SOCIAL_AUTH_PIPELINE = [
-    # Get the information we can about the user and return it in a simple
-    # format to create the user instance later. On some cases the details are
-    # already part of the auth response from the provider, but sometimes this
-    # could hit a provider API.
-    "social_core.pipeline.social_auth.social_details",
-    # Get the social uid from whichever service we're authing thru. The uid is
-    # the unique identifier of the given user in the provider.
-    "social_core.pipeline.social_auth.social_uid",
-    # Verifies that the current auth process is valid within the current
-    # project, this is where emails and domains whitelists are applied (if
-    # defined).
-    # 'social_core.pipeline.social_auth.auth_allowed',
-    # Checks if the current social-account is already associated in the site.
-    "social_core.pipeline.social_auth.social_user",
-    # Make up a username for this person, appends a random string at the end if
-    # there's any collision.
-    "social_core.pipeline.user.get_username",
-    # Send a validation email to the user to verify its email address.
-    # 'social_core.pipeline.mail.mail_validation',
-    # Associates the current social details with another user account with
-    # a similar email address.
-    "social_core.pipeline.social_auth.associate_by_email",
-    # Create a user account if we haven't found one yet.
-    "social_core.pipeline.user.create_user",
-    # Create the record that associated the social account with this user.
-    "social_core.pipeline.social_auth.associate_user",
-    # Populate the extra_data field in the social record with the values
-    # specified by settings (and the default ones like access_token, etc).
-    "social_core.pipeline.social_auth.load_extra_data",
-    # Update the user record with any changed info from the auth service.
-    "social_core.pipeline.user.user_details",
-]
